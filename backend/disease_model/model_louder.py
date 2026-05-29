34dc891be7740450b8ff6a29afc8d28a806c91bc
Gaganjeet Singh <gagandhiman870@gmail.com>
2026-05-29 11:03:09 +0530

    add tensorflow back

--- backend/disease_model/model_louder.py
+++ backend/disease_model/model_louder.py
@@ -7,7 +7,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, "plant_disease_model.h5")
 ENCODER_PATH = os.path.join(BASE_DIR, "label_encoder.pkl")
 
-model = tf.keras.models.load_model(MODEL_PATH)
+model = None
+le = None
 
-with open(ENCODER_PATH, "rb") as f:
-    le = pickle.load(f)
+if os.path.exists(MODEL_PATH):
+    model = tf.keras.models.load_model(MODEL_PATH)
+
+if os.path.exists(ENCODER_PATH):
+    with open(ENCODER_PATH, "rb") as f:
+        le = pickle.load(f)
